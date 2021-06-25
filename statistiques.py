0a3f2907600fa04e8b7c9ec1b393c17221de0605
MachaBar <79354669+MachaBar@users.noreply.github.com>
2021-06-25 13:56:22 +0200

    Add files via upload

--- statistiques.py
+++ statistiques.py
@@ -4,6 +4,7 @@ import numpy as np
 import math 
 import matplotlib.pyplot as plt
 import matplotlib.tri as tri 
+import pickle
 from tempfile import TemporaryFile
 from wordcloud import WordCloud, STOPWORDS
 from PIL import Image
@@ -13,7 +14,7 @@ from ouvertureFichiers import lecture_fichier
 from preprocessing import preprocessing, BoW, document_frequency, matriceX_Dico_processed, inverse_doc_frequency, remove_keys, matriceX_Dico_processed_inverse, mots_recurrents
 from creationMatricesDistances import matrice_DistancesEucl, matrice_DistancesEucl_version, matrice_DistancesCos_version, matrice_DistancesCos
 from utilitaire import split
-from perceptron import ajout_colonne_de1, labels, epoque2, cout_L, perceptron_epoque, perceptron, graphe, z_fonction, retrouveSerie, performance
+from perceptron import ajout_colonne_de1, labels, epoque2, cout_L, perceptron_epoque, perceptron, graphe, retrouveSerie, performance
 
 
 def histo(M):
@@ -68,10 +69,18 @@ def wordcloudPreproc(L):
     plt.axis("off")
     plt.show()
 
-def wordcloudDocFreq(X,D,docFreq,L):
+"""def wordcloudDocFreq(X,D,docFreq,L):
     ListeIndiceToDelete = [ i for i in range(0,len(docFreq)) if (docFreq[i] > 0.9) or (docFreq[i] < 3/len(X))]
     ListeMotsToDelete = [ cle for cle, valeur in D.items() if (valeur in ListeIndiceToDelete) ]
     R = [ e for s in L for e in s if (e not in ListeMotsToDelete)]
+    text = " ".join(R)
+    wordcloud = WordCloud(background_color = 'white', stopwords = [] , max_words = 50).generate(text)
+    plt.imshow(wordcloud)
+    plt.axis("off")
+    plt.show() """
+    
+def wordcloudInvDocFreq(X,D,invDocFreq):
+    R = mots_recurrents(X,D,invDocFreq)
     text = " ".join(R)
     wordcloud = WordCloud(background_color = 'white', stopwords = [] , max_words = 50).generate(text)
     plt.imshow(wordcloud)
@@ -82,6 +91,13 @@ def wordcloudDocFreq(X,D,docFreq,L):
     
     
 T,Y = lecture_fichier("/home/baranova/Bureau/L2/LU2IN013/series_3")
+
+data = np.load("series_3.npz")
+X = data['name1']
+with open("myDictionary_3series.pkl", "rb") as tf:
+    dico = pickle.load(tf)
 #print( wordcloudLecFic("/home/baranova/Bureau/L2/LU2IN013/series_3"))
-print(wordcloudPreproc(preprocessing(T)))
+#print(wordcloudPreproc(preprocessing(T)))
+
+print(wordcloudInvDocFreq(X, dico, inverse_doc_frequency(X)))
 
